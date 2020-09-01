2f7619d44b87f17bf343d1ef78da7064e94e4598
R. Richards <rdrrichards@hotmail.com>
2020-09-01 17:40:21 -0500

    pivot of dataset to table

--- src/app/bar-chart/bar-chart-data.service.ts
+++ src/app/bar-chart/bar-chart-data.service.ts
@@ -27,13 +27,39 @@ export class BarChartDataService {
     { label: '2020-08-19', value: 223 },
     { label: '2020-08-20', value: 75 }
   ];
+  private userData: ChartData[] = [
+    { label: '2020-08-01', value: 220, auditBy: 'RR1' },
+    { label: '2020-08-02', value: 223, auditBy: 'RR1' },
+    { label: '2020-08-03', value: 210, auditBy: 'RR1' },
+    { label: '2020-08-04', value: 190, auditBy: 'RR1' },
+    { label: '2020-08-05', value: 195, auditBy: 'RR3' },
+    { label: '2020-08-01', value: 199, auditBy: 'RR3' },
+    { label: '2020-08-02', value: 202, auditBy: 'RR3' },
+    { label: '2020-08-03', value: 209, auditBy: 'RR3' },
+    { label: '2020-08-04', value: 210, auditBy: 'RR2' },
+    { label: '2020-08-05', value: 205, auditBy: 'RR2' },
+    { label: '2020-08-01', value: 222, auditBy: 'RR2' },
+    { label: '2020-08-02', value: 221, auditBy: 'RR2' },
+    { label: '2020-08-03', value: 199, auditBy: 'RR2' },
+    { label: '2020-08-04', value: 188, auditBy: 'RR2' },
+    { label: '2020-08-05', value: 175, auditBy: 'RR1' },
+    { label: '2020-08-01', value: 123, auditBy: 'RR3' },
+    { label: '2020-08-02', value: 230, auditBy: 'RR1' },
+    { label: '2020-08-03', value: 232, auditBy: 'RR2' },
+    { label: '2020-08-04', value: 223, auditBy: 'RR1' },
+    { label: '2020-08-05', value: 75, auditBy: 'RR1' }
+  ];
   constructor() { }
   getChartData(): Observable<ChartData[]> {
     return of(this.data);
+  }
+  getUserChartData(): Observable<ChartData[]> {
+    return of(this.userData);
   }
 }
 
 export interface ChartData {
   label: string;
+  auditBy?: string;
   value: number;
 }
--- src/app/bar-chart/bar-chart.component.ts
+++ src/app/bar-chart/bar-chart.component.ts
@@ -11,6 +11,8 @@ export class BarChartComponent implements OnInit {
   data: any;
   options: any;
   chartData$: Observable<ChartData[]>;
+  userData: any[] = [];
+  cols: string[] = [];
   constructor(private barChartDataService: BarChartDataService) { }
   ngOnInit(): void {
     this.chartData$ = this.barChartDataService.getChartData();
@@ -39,6 +41,22 @@ export class BarChartComponent implements OnInit {
             }]
         }
     };
+    this.barChartDataService.getUserChartData().subscribe(data => {
+      this.cols = [ 'label', ...new Set(data.map(a => a.auditBy).sort())];
+      // console.log('this.cols', this.cols);
+      const pivot = []; // {label: '2020-08-20', RR1: 75, RR2: 200, RR3: 175, }
+      data.map(b => {
+        const pRow = pivot.find(c => c.label === b.label);
+        if (pRow) {
+          pRow[b.auditBy] = b.value;
+        } else {
+          const newRow = { label: b.label };
+          newRow[b.auditBy] = b.value;
+          pivot.push(newRow);
+        }
+      });
+      this.userData = pivot;
+    });
   }
 
 }
